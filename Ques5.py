622923ff4bb47bbf8b596bac196b6d2613d75792
Karan Rai <103351072+EngineerKaran@users.noreply.github.com>
2022-04-25 10:10:03 +0530

    Delete Ques5.py

--- Ques5.py
+++
@@ -1,8 +0,0 @@
-print("Welcome!")
-print("\nList items Deletion Program")
-color = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
-color = [x for (i,x) in enumerate(color) if i in (0,1,2,4,5)]
-print("\n")
-print(color)
-color = [x for (i,x) in enumerate(color) if i in (0,1,2,5)]
-print(color)
